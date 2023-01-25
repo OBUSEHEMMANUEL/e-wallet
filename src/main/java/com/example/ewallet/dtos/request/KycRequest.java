33dd6dc022f6020cef3ec5bada3fdced4d694387
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-01-26 00:36:51 +0100

    added more fields to KycRequest Dto

--- src/main/java/com/example/ewallet/dtos/request/KycRequest.java
+++ src/main/java/com/example/ewallet/dtos/request/KycRequest.java
@@ -1,7 +1,25 @@
 package com.example.ewallet.dtos.request;
 
+import com.example.ewallet.data.models.CardType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 
 @Data
 public class KycRequest {
+    private String bvn;
+    private String cardNo;
+    private String CardName;
+    private String expireDate;
+    private int cvv;
+    private String nextOfKinFullName;
+    private String emailAddress;
+    private String phoneNumber;
+    private String relationship;
+    private String homeAddress;
+    private CardType cardType;
 }
