34823e5ac4330a998f9882036286a8643335ae11
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-01-26 00:34:45 +0100

    added new fields to Kyc

--- src/main/java/com/example/ewallet/data/models/Kyc.java
+++ src/main/java/com/example/ewallet/data/models/Kyc.java
@@ -1,37 +1,52 @@
 package com.example.ewallet.data.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.Date;
+
 
 @Data
+@Builder
 @Document
 @AllArgsConstructor
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class Kyc {
 
-    @Id
-    private Long bvn;
-    @NotBlank(message = "Wrong card number format")
-    @Pattern(regexp = "\\d{10}")
-    private Long cardNo;
-    @NotBlank(message = "Card name cannot be blank")
-    private Long CardName;
     @NotBlank
-    private LocalDateTime expireDate;
+    private String userId;
+    @Id
+    private String id;
+    @NotBlank(message = "Bvn is required")
+    @Pattern(regexp = "\\d{11}")
+    private String bvn;
+    @NotBlank(message = "Wrong card number format")
+    @Pattern(regexp = "\\d{16}")
+    private String cardNo;
+    @NotBlank(message = "Card name cannot be blank")
+    private String CardName;
+    @NotBlank
+    private String expireDate;
     @NotBlank(message = "Wrong cvv format")
     @Pattern(regexp = "\\d{3}")
     private int cvv;
-    private String nextOfFullName;
+    @NotBlank(message = "Name cannot be blank")
+    private String nextOfKinFullName;
+    @NotBlank(message = "Wrong email format")
     private String emailAddress;
+    @NotBlank(message = "Wrong phone number format")
+    @Pattern(regexp = "\\d{11}")
     private String phoneNumber;
+    @NotBlank
     private String relationship;
+    @NotBlank(message = "Address field is required")
+    @Pattern(regexp = "^[a-zA-Z0-9\\\\s,'-]*$")
+    private String homeAddress;
+    @NotBlank(message = "Select card type")
+    private CardType cardType;
 }
