c1cfee6d540e24841c5b602aa67ab3ca7ab61588
timi <adeloduntimilehin15@gmail.com>
2023-01-26 14:19:57 +0100

    updated User model

--- src/main/java/com/example/ewallet/data/models/User.java
+++ src/main/java/com/example/ewallet/data/models/User.java
@@ -6,7 +6,13 @@ import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Document
 @Data
@@ -28,4 +34,6 @@ public class User {
     private String password;
     private boolean isDisabled = true;
     private boolean isCompletedKyc;
+    @DBRef
+    private Set<Card> userCards = new HashSet<>();
 }
