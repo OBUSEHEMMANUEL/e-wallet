d9a99b88b79b97079f9117f4c960d9640f690baf
timi <adeloduntimilehin15@gmail.com>
2023-01-26 14:19:18 +0100

    added CardServiceImpl

---
+++ src/main/java/com/example/ewallet/service/CardServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.ewallet.service;
+
+import com.example.ewallet.data.models.Card;
+import com.example.ewallet.data.repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CardServiceImpl implements CardService{
+    @Autowired
+    private CardRepository cardRepository;
+    @Override
+    public void addCard(Card card) {
+        cardRepository.save(card);
+    }
+
+    @Override
+    public Optional<Card> findCard(String cardNo) {
+        return cardRepository.findByCardNo(cardNo);
+    }
+}
