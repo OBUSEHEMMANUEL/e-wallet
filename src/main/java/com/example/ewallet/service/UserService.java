d90ae0b0489de81a1f9083db300cc37aa571b346
timi <adeloduntimilehin15@gmail.com>
2023-01-26 14:20:51 +0100

    added "addCard" method

--- src/main/java/com/example/ewallet/service/UserService.java
+++ src/main/java/com/example/ewallet/service/UserService.java
@@ -1,8 +1,10 @@
 package com.example.ewallet.service;
 
 import com.example.ewallet.data.models.User;
+import com.example.ewallet.dtos.request.AddCardRequest;
 import com.example.ewallet.dtos.request.ChangePasswordRequest;
 import com.example.ewallet.dtos.request.LoginRequest;
+import com.example.ewallet.dtos.response.AddCardResponse;
 import com.example.ewallet.dtos.response.LoginResponse;
 
 public interface UserService {
@@ -14,4 +16,5 @@ public interface UserService {
     String changePassword(ChangePasswordRequest changePasswordRequest);
 
    String  generateToken(User user);
+   AddCardResponse addCard(AddCardRequest addCardRequest);
 }
