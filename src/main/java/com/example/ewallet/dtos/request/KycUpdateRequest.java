902cc9b969fd9cdc5e0f851b83372db9120cd288
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-01-26 09:30:39 +0100

    updated KycUpdateRequest Dto field with next of kin and card features

--- src/main/java/com/example/ewallet/dtos/request/KycUpdateRequest.java
+++ src/main/java/com/example/ewallet/dtos/request/KycUpdateRequest.java
@@ -1,21 +1,17 @@
 package com.example.ewallet.dtos.request;
 
+import com.example.ewallet.data.models.Card;
 import com.example.ewallet.data.models.CardType;
+import com.example.ewallet.data.models.NextOfKin;
 import lombok.Data;
 
 @Data
 public class KycUpdateRequest {
     private String userId;
     private String kycId;
-    private String cardNo;
     private String password;
-    private String CardName;
-    private String expireDate;
-    private int cvv;
-    private String nextOfKinFullName;
-    private String emailAddress;
-    private String phoneNumber;
-    private String relationship;
+    private NextOfKin nextOfKin;
     private String homeAddress;
+    private Card card;
     private CardType cardType;
 }
